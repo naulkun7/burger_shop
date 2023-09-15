d5b2749f6d6b85a69fdfe740ecc68da6259c24c6
naulkun7 <luannguyenn.2k2@gmail.com>
2023-09-16 01:42:04 +0700

    setup btn tag

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,30 +1,31 @@
 import React from "react"
 import css from "./index.module.scss"
 import { Link } from "react-router-dom"
-import logoImg from "../../assets/images/Jevelin_logo_dark.png"
 
 function index() {
   return (
     <div id="header" className={css.header}>
-      <img src={logoImg} alt="logo" />
-      <div className={css.navbar}>
-        <Link className={css.navbar__item} to="/">
+      <Link className={css.header__logo} to="/">
+        burger
+      </Link>
+      <div className={css.header__navbar}>
+        <Link className={css.header__navbar__item} to="/">
           Home
         </Link>
 
-        <Link className={css.navbar__item} to="/">
+        <Link className={css.header__navbar__item} to="/">
           About
         </Link>
 
-        <Link className={css.navbar__item} to="/">
+        <Link className={css.header__navbar__item} to="/">
           Offer
         </Link>
 
-        <Link className={css.navbar__item} to="/">
+        <Link className={css.header__navbar__item} to="/">
           Home
         </Link>
 
-        <Link className={css.navbar__item} to="/">
+        <Link className={css.header__navbar__item} to="/">
           Contact
         </Link>
       </div>
